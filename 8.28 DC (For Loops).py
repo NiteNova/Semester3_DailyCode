edd36163deb05fee060eeaba5a70a26ad673d3bb
NiteNova <112674365+NiteNova@users.noreply.github.com>
2023-08-28 14:39:10 -0600

    Create 8.28 DC (For Loops).py

---
+++ 8.28 DC (For Loops).py
@@ -0,0 +1,37 @@
+for i in range (70, 80, 1):
+    print(i)
+for g in range(100, 301, 20):
+    print(g)
+for t in range(25, -26, -5):
+    print(t)
+
+#spicy
+for a_sequence in range (1, 10):
+    print(3+2*(a_sequence-1))
+
+for a in range(1, 11):
+    print(-5+9*(a-1))
+
+for p in range(1, 8):
+    print(20-17*(p-1))
+
+for n in range(1, 21):
+    print(2+0.4*(n-1))
+
+b = -5
+print(b)
+for o in range(2, 5):
+    b = b+9
+    print(b)
+
+c = 20
+print(c)
+for ii in range(2, 4):
+    c = c-17
+    print(c)
+
+d = 2
+print(d)
+for _ in range(2,6):
+    d= d+0.4
+    print(d)
